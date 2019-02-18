63390f43ba4a983888066f691468fc6db76121de
Richard Millet <remillet@gmail.com>
2019-02-18 15:35:15 -0800

    NOJIRA: Config webpack and webpack devserver.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import path from 'path';
+import log from './log';
+
+console.log("__filename is ", __filename)
+
+log('Hello React?')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	node: {
+		__filename: true
+	},
+	context: path.join(__dirname, 'src'),
+    mode: 'development',
     entry: './index.js',
     output: {
-        path: __dirname,
+        path: path.join(__dirname, 'dist'),
         filename: './bundle.js'
     },
     module: {
@@ -13,5 +20,11 @@ module.exports = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+    	new HtmlWebpackPlugin({
+    		template: './index.html',
+    		inject: 'body'
+    	})
+    ]
 };
