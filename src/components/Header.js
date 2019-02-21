614c74d5ed042cfa4ff0663283e4f09d2c67ebc1
Richard Millet <remillet@gmail.com>
2019-02-20 16:43:22 -0800

    NOJIRA: Styling using postcss config is now working.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => {
     return(
         <header>
-            <h1>My Recipes</h1>
+            <h1 className="red">My Recipes</h1>
         </header>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader'
             },
             {
