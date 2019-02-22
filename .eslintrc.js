e3c3bf72a9f31a080fa7860064907d2e9c1da1e2
Richard Millet <remillet@gmail.com>
2019-02-21 18:28:43 -0800

    NOJIRA: Lint cleaned all modules and added React prop types.

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    // "extends": "eslint:recommended",
+    "globals": {
+        "API_URL": true,
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-console": ["error", { allow: ["warn", "error"] }],
+        "react/jsx-filename-extension": 0,
+        "react/jsx-one-expression-per-line": ["off", { "allow": "literal" }],
+        // Allow object propTypes
+        "react/forbid-prop-types": ["error", { "forbid": ["any", "array"] }],
+        "jsx-a11y/no-noninteractive-element-interactions": [
+          "off",
+          {
+            handlers: [
+              "onClick",
+              "onMouseDown",
+              "onMouseUp",
+              "onKeyPress",
+              "onKeyDown",
+              "onKeyUp",
+            ],
+          },
+        ],
+        "jsx-a11y/click-events-have-key-events": 0,
+    }
+};
