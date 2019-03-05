c9f896e92d2689148eeb16a45237df5a061ae197
Richard Millet <remillet@gmail.com>
2019-03-05 13:53:15 -0800

    NOJIRA: Added additional Jest-based tests.

--- src/specs/RecipeListItemSpec.js
+++ src/specs/RecipeListItemSpec.js
@@ -2,6 +2,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import RecipeListItem from '../components/RecipeListItem';
 
+const testRecipe = {
+  id: 1,
+  name: 'Test Recipe',
+  category: 'Test category',
+};
+
+const testFavorites = [1];
+
 describe('<RecipeListItem />', () => {
   test('Should not break if no recipe/details passed in', () => {
     const component = renderer.create(<RecipeListItem />);
@@ -10,8 +18,18 @@ describe('<RecipeListItem />', () => {
   });
 
   test('Should correctly render recipe', () => {
+    const component = renderer.create(
+      <RecipeListItem recipe={testRecipe} />,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   test('Should render favorite state', () => {
+    const component = renderer.create(
+      <RecipeListItem recipe={testRecipe} favorites={testFavorites} />,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
